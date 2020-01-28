9a48b26f2b4c89a1b5f7c348a9095b808fa723cb
Student <example@example.com>
2020-01-28 11:56:12 -0800

    add post route specs

--- spec/requests/get_reviews_spec.rb
+++ spec/requests/get_reviews_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe "get all reviews route", :type => :request do
-  let!(:reviews) { FactoryBot.create_list(:review, 20)}
+  let!(:reviews) { FactoryBot.create_list(:review, 19)}
 
   before { get '/reviews'}
 
   it 'returns all reviews' do
-    expect(JSON.parse(response.body).size).to eq(20)
+    expect(JSON.parse(response.body).size).to eq(19)
   end
 
   it 'returns status code 200' do
